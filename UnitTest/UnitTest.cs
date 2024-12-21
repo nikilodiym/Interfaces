f5df30283dff795bb9221cddc93b2ca8edee4800
Nikita Krenov <nikitabaram@gmail.com>
2024-12-21 13:17:55 +0200

    Refactor namespaces and update unit tests
    Removed `Class1` from `Interfaces` namespace.
    Added new `UnitTest` class with placeholder method in `UnitTest` namespace.
    Removed `UnitTest1` class and its `TestMethod1` method from `UnitTest` namespace.

---
+++ UnitTest/UnitTest.cs
@@ -0,0 +1,12 @@
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        // Uncomment the following code to test the method
+        //[TestMethod]
+        //public void TestMethod1()
+        //{
+        //}
+    }
+}
